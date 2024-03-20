74d544136590aa2f1147ed1acd3224b6b3ea05c6
PavinduChamikara <112230998+PavinduChamikara@users.noreply.github.com>
2024-03-20 11:56:39 +0530

    Update Portfolio.jsx

--- src/componenets/Portfolio.jsx
+++ src/componenets/Portfolio.jsx
@@ -6,25 +6,31 @@ const Portfolio = () => {
     const portfolios = [
         {
             id: 1,
-            src: portImage
+            src: portImage,
         },
         {
             id: 2,
-            src: portImage
+            src: portImage,
         },
         {
             id: 3,
-            src: portImage
+            src: portImage,
         },
         {
             id: 4,
-            src: portImage
+            src: portImage,
         },
-    ]
+        {
+            id: 5,
+            src: portImage,
+        },
+        {
+            id: 6,
+            src: portImage,
+        },
+    ];
 
     return (
-
-
     <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
@@ -35,7 +41,7 @@ const Portfolio = () => {
 
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
             {
-                portfolios.map(({id, src}) => {
+                portfolios.map(({id, src}) => (
                 <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
                     <img src={src} alt="" className='rounded-md duration-200 hover:scale-105' />
                     <div className='flex items-center justify-center'>
@@ -43,7 +49,7 @@ const Portfolio = () => {
                         <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
                     </div>
                 </div>
-                })
+                ))
             }
             </div>
 
